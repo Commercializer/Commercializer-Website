62377b82a51081c0f02eb873b7b5e14a5e67f36b
Nethsara Sandeepa Elvitigala <nethrenial@HaulMatics-MacBook-Air.local>
2024-09-30 21:46:22 +0530

    refactor: remove old project

--- README.md
+++
@@ -1,1 +0,0 @@
-# Commercializer Website Beta
--- plugins/vue-the-mask.ts
+++
@@ -1,6 +0,0 @@
-import VueTheMask from 'vue-the-mask'
-
-export default defineNuxtPlugin((nuxtApp) => {
-    // @ts-ignore
-    nuxtApp.vueApp.use(VueTheMask)
-})
