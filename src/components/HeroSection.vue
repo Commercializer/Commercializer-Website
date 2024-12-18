fc4c4a297d113b2294869780b55a02e0a23e10cd
AamirArshad <aamirfazeer@gmail.com>
2024-12-19 00:16:41 +0530

    feat: develop GSAP Animation

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,18 +2,20 @@
   <section class="position-relative overflow-hidden zindex-2" id="home">
     <b-container style="min-height: calc(100vh - 74px)">
       <b-row
-        class="justify-content-center align-items-center"
+        class="justify-content-center align-items-center hero-row"
         style="min-height: calc(100vh - 120px)"
       >
         <!-- Parallax gfx -->
-        <b-col xl="6" lg="6" md="6" sm="8" class="col-10 offset-xl-1 order-md-2">
+        <b-col xl="6" lg="6" md="6" sm="8" class="col-10 offset-xl-1 order-md-2 image-col">
           <div class="triangle-container">
             <img
-              :src= "logo"
+              src="@/assets/img/logo-large.svg"
+              ref="logo"
               alt="Triangle"
               class="center-triangle"
+              :class="{ 'pulse-animation': isLogoPulsing }"
+              id="center-triangle"
             />
-
             <!-- Circular Cards -->
             <div
               v-for="(card, index) in cards"
@@ -31,7 +33,7 @@
         </b-col>
 
         <!-- Text -->
-        <b-col lg="5" md="6" class="text-center text-md-start order-md-1">
+        <b-col lg="5" md="6" class="text-center text-md-start order-md-1  text-col">
           <h1 class="display-5 mb-lg-4 shimmer">
             Commercializing Innovations
           </h1>
@@ -80,56 +82,194 @@
         </b-col>
       </b-row>
     </b-container>
+    <section class="below-section" id="below">
+      <div  class="logo-parts-container" ref="logoPartsContainer">
+                <div class="logo-part part-1" ref="part1">
+                  <img src= "@/assets/img/Asset_L35.svg">
+                  <div class="logo-part-text">
+                      <h3 class="logo-part-title part-1-text">Innovation</h3>
+                  </div>
+                </div>
+                <div class="logo-part part-2" ref="part2">
+                  <img src= "@/assets/img/Asset_L36.svg">
+                    <div class="logo-part-text">
+                           <h3 class="logo-part-title part-2-text">Experience</h3>
+                    </div>
+                  </div>
+                <div class="logo-part part-3" ref="part3">
+                  <img src= "@/assets/img/Asset_L37.svg">
+                    <div class="logo-part-text">
+                         <h3 class="logo-part-title part-3-text">Commercial Strategy</h3>
+                        </div>
+                  </div>
+            </div>
+    </section>
   </section>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { ref, onMounted, onBeforeUnmount, nextTick } from 'vue';
 import ChevronIcon from '@iconify/icons-bx/bx-chevrons-down';
 import { Icon } from '@iconify/vue';
-import logo from '@/assets/img/logo-large.svg';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const logo = ref<HTMLImageElement | null>(null);
+const logoPartsContainer = ref<HTMLElement | null>(null);
+const part1 = ref<HTMLElement | null>(null);
+const part2 = ref<HTMLElement | null>(null);
+const part3 = ref<HTMLElement | null>(null);
 
 const cards = ref([
-  { title: 'Product Studio', text: 'Build Your Dream Product' },
-  { title: 'AI Automation Agency', text: 'Streamline Processes with AI' },
-  { title: 'UX Studio', text: 'Design Stunning Experiences' },
+    { title: 'Product Studio', text: 'Build Your Dream Product' },
+    { title: 'AI Automation Agency', text: 'Streamline Processes with AI' },
+    { title: 'UX Studio', text: 'Design Stunning Experiences' },
 ]);
 
 const activeIndex = ref(0);
+const isLogoPulsing = ref(true);
 
 const getCardPosition = (index: number) => {
-  const screenWidth = window.innerWidth;
+    const screenWidth = window.innerWidth;
 
-  // Define default positions for larger screens
-  const desktopPositions = [
-    { left: '16%', top: '0%' },  // Active card
-    { left: '86%', top: '38%' }, 
-    { left: '24%', top: '100%' },
-  ];
+    // Define default positions for larger screens
+    const desktopPositions = [
+        { left: '16%', top: '0%' },  // Active card
+        { left: '86%', top: '38%' },
+        { left: '24%', top: '100%' },
+    ];
 
-  // Define positions for mobile screens 
-  const mobilePositions = [
-    { left: '-7%', top: '10%' },  // Active card
-    { left: '6%', top: '101%' }, 
-    { left: '68%', top: '40%' }, 
-  ];
+    // Define positions for mobile screens
+    const mobilePositions = [
+        { left: '-7%', top: '10%' },  // Active card
+        { left: '6%', top: '101%' },
+        { left: '68%', top: '40%' },
+    ];
 
-  if (screenWidth <= 768) {
-    return mobilePositions[(index - activeIndex.value + mobilePositions.length) % mobilePositions.length];
-  }
+    if (screenWidth <= 768) {
+        return mobilePositions[(index - activeIndex.value + mobilePositions.length) % mobilePositions.length];
+    }
 
-  const positionIndex = (index - activeIndex.value + desktopPositions.length) % desktopPositions.length;
-  return desktopPositions[positionIndex];
+    const positionIndex = (index - activeIndex.value + desktopPositions.length) % desktopPositions.length;
+    return desktopPositions[positionIndex];
 };
 
-
 let intervalId: ReturnType<typeof setInterval>;
+let tl : gsap.core.Timeline;
 
-onMounted(() => {
-  intervalId = setInterval(() => {
-    activeIndex.value = (activeIndex.value + 1) % cards.value.length;
-  }, 4500); 
 
+onMounted( async () => {
+     await nextTick();
+    intervalId = setInterval(() => {
+        activeIndex.value = (activeIndex.value + 1) % cards.value.length;
+    }, 4500);
+
+    tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".below",
+            start: "top top",
+            end: "bottom top",
+            scrub: true,
+            onUpdate: (self) => {
+                 isLogoPulsing.value = self.progress === 0;
+
+                if(self.progress > 0 && !isLogoPulsing.value) {
+                    handleLogoBreak();
+                }else{
+                    handleLogoReassemble();
+                }
+            },
+             markers: false,
+        },
+    });
+
+    tl.to(
+        ".center-triangle",
+        {
+            scale: 1.4,
+            x: "-120%",
+            y: "240%",
+            rotation: 360,
+            ease: "power2.inOut",
+            duration: 1.2,
+              opacity:0,
+        },
+        0
+    )
+
+
+    // Add a subtle fade effect on the text content
+    tl.fromTo([".text-col h1", ".text-col p", ".text-col .locations",".discover-more"],{
+    }, {
+        opacity: 0,
+        duration: 0.4,
+        ease: "power1.inOut"
+    }, 0)
+
+const handleLogoBreak = () => {
+  if (logoPartsContainer.value && logo.value && part1.value && part2.value && part3.value) {
+      gsap.set([logoPartsContainer.value], {display:'flex'})
+    gsap.to([part1.value], {
+      top: '70vh',
+      left: '10vw',
+      opacity: 1,
+      duration: 0.6,
+      ease: "power2.inOut",
+      onComplete:()=>{
+       gsap.to([".part-1-text"],{opacity:1, duration:0.4})
+      }
+      });
+    gsap.to([part2.value], {
+      top: '85vh',
+      left: '45vw',
+      opacity: 1,
+      duration: 0.6,
+      ease: "power2.inOut",
+      onComplete:()=>{
+       gsap.to([".part-2-text"],{opacity:1, duration:0.4})
+      }
+    });
+    gsap.to([part3.value], {
+      top: '70vh',
+      left: '80vw',
+      opacity: 1,
+      duration: 0.6,
+      ease: "power2.inOut",
+       onComplete:()=>{
+         gsap.to([".part-3-text"],{opacity:1, duration:0.4})
+       }
+    });
+  }
+};
+const handleLogoReassemble = () => {
+  if (logoPartsContainer.value && logo.value && part1.value && part2.value && part3.value) {
+    gsap.to([".part-1-text",".part-2-text",".part-3-text"],{opacity:0,duration:0.4})
+    gsap.to([part1.value], {
+      top: '13vh',
+      left:  '41vw',
+      opacity: 1,
+      duration: 0.6,
+      ease: "power2.inOut",
+      });
+      gsap.to([part2.value], {
+        top:  '49vh',
+        left:  '35.9vw' ,
+        opacity: 1,
+        duration: 0.6,
+        ease: "power2.inOut",
+      });
+      gsap.to([part3.value], {
+          top: '31.5vh',
+          left: '53vw',
+          opacity: 1,
+          duration: 0.6,
+          ease: "power2.inOut",
+    });
+        gsap.set([logoPartsContainer.value], {display:'none', delay:0.6})
+    }
+};
 });
 
 onBeforeUnmount(() => {
@@ -138,6 +278,9 @@ onBeforeUnmount(() => {
 </script>
 
 <style scoped>
+#home {
+  overflow: visible;
+}
 
 .hero-section {
   min-height: calc(100vh - 74px);
@@ -171,17 +314,88 @@ onBeforeUnmount(() => {
 }
 
 @keyframes shimmer {
-		0% {
-      background-position: -100% 0;
-		}
-		100% {
-      background-position: 100% 0;
-		}
+  0% {
+    background-position: -100% 0;
+  }
+  100% {
+    background-position: 100% 0;
+  }
+}
+
+.center-triangle.pulse-animation {
+  animation: pulse 4s infinite ease-in-out;
+}
+.logo-parts-container{
+    display: none;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    pointer-events: none;
+}
+.logo-part{
+     position: absolute;
+     pointer-events: none;
+     opacity: 1;
+     display: flex;
+     flex-direction: column;
+}
+.logo-part.part-1{
+    width: 12vw;
+    height: 36vh;
+    top: 13vh;
+    left: 41vw;
+}
+.logo-part.part-2{
+    width: 18vw;
+    height: 20vh;
+    top: 49vh;
+    left: 35.9vw;
+}
+.logo-part.part-3{
+    width: 10vw;
+    height: 24vh;
+    top: 31.5vh;
+    left: 53vw;
+}
+.logo-part svg{
+    position: absolute;
+    max-width: 100%;
+    max-height: 100%;
+}
+.logo-part-text{
+    color: #fff;
+    margin-top: 15%;
+    text-align: center;
+}
+.logo-part-title{
+    font-size: 1.5rem;
+    margin-bottom: 0.5rem;
+    opacity: 0;
+}
+.part-1-text {
+  margin-top: 10%;
+}
+.part-2-text{
+  margin-top: 10%;
+}
+.part-3-text{
+  margin-top:10%;
 }
 
 
+@keyframes fade-in {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
 @keyframes pulse {
-  0%, 100% {
+  0%,
+  100% {
     transform: scale(1);
     opacity: 0.9;
   }
@@ -193,24 +407,31 @@ onBeforeUnmount(() => {
 
 .triangle-container {
   position: relative;
-  display: flex; /* Stack cards below the logo */
-  align-items: right; 
+  display: flex;
+  align-items: right;
   justify-content: right;
-  margin-bottom: 10% ;
+  margin-bottom: 10%;
   width: 100%;
   max-width: 500px;
   z-index: 3;
+}
+.cls-1{
+    fill: url(#linear-gradient-1);
+}
+.cls-2{
+    fill: url(#linear-gradient-2);
+}
+.cls-3{
+    fill: url(#linear-gradient-3);
 }
 
 .center-triangle {
   width: 100%;
   max-width: 300px;
-  z-index: 4;
-  transform: translate(-10%, -10%); 
-  transform-origin: center; 
-  animation: pulse 4s infinite ease-in-out; /* Apply pulse animation */
+  z-index: 10;
+  position: relative;
+  /* animation: pulse 4s infinite ease-in-out; */
 }
-
 
 .box-card-wrapper {
   position: absolute;
@@ -220,7 +441,7 @@ onBeforeUnmount(() => {
   width: 44%;
   height: 24%;
   border-radius: 12px;
-  transition: all 0.8s ease-in-out; 
+  transition: all 0.6s ease-in-out;
   z-index: 1;
   opacity: 0.5;
   background: radial-gradient(
@@ -239,16 +460,17 @@ onBeforeUnmount(() => {
 
 .box-card-content {
   position: relative;
-  z-index: 2;
+  z-index: 1;
   width: 100%;
   height: 100%;
-  border-radius: 8px; 
+  border-radius: 8px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
+  
 }
 
 .box-card-content h4 {
@@ -263,7 +485,7 @@ onBeforeUnmount(() => {
 
 @media (max-width: 768px) {
   .triangle-container {
-    align-items: center; 
+    align-items: center;
     justify-content: center;
     max-width: 100%;
     margin-bottom: 20%;
@@ -293,5 +515,11 @@ onBeforeUnmount(() => {
   .box-card-content p {
     font-size: 0.4rem;
   }
+  .logo-part-title{
+    font-size: 1.2rem;
+}
+}
+.below-section{
+    height: 150vh;
 }
 </style>
