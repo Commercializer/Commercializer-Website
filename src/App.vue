26804a1f3e599a37732402a1be66672ec1d0c7fc
AamirArshad <aamirfazeer@gmail.com>
2025-02-11 08:42:28 +0530

    fix: comment out the cookie banner

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <RouterView />
-  <CookieBanner />
+  <!-- <CookieBanner v-if="bannerMounted" /> -->
 </template>
 
 <script lang="ts" setup>
 import { RouterView } from 'vue-router'
-import { onMounted } from 'vue'
+import { onMounted, ref} from 'vue'
 import CookieBanner from '@/components/CookieBanner.vue'
 
 import SmoothScroll from 'smooth-scroll'
 
+const bannerMounted = ref(false)
+
 onMounted(() => {
   var scroll = new SmoothScroll('a[href*="#"]')
+  setTimeout(() => {
+    bannerMounted.value = true
+  }, 50) 
 })
 </script>
